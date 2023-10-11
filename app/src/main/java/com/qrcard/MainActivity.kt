9f36601410056866fc3f0f210c74c541c6384fa7
SaileSC <sailesantos0@gmail.com>
2023-10-10 21:49:59 -0400

    Initial commit

---
+++ app/src/main/java/com/qrcard/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.qrcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.google.android.material.tabs.TabLayout
+import kotlin.properties.Delegates
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_item)
+    }
+}
